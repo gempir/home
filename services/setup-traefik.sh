5e2d60fdca76789ae7de4b017151cf0f0515d2d8
gempir <gempir.dev@gmail.com>
2022-12-01 22:02:21 +0100

    switch to network

--- services/setup-traefik.sh
+++ services/setup-traefik.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ip link add traefik-shim link eth0 type macvlan mode bridge
-ip addr add 192.168.178.194/32 dev traefik-shim
+ip addr add 192.168.1.194/32 dev traefik-shim-new
 ip link set traefik-shim up
-ip route add 192.168.178.192/27 dev traefik-shim
+ip route add 192.168.1.192/27 dev traefik-shim
